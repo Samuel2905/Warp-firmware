dfd1fc1cda7caf222fdced2207f75937c3a7ba71
Samuel2905 <samuelclarke2905@yahoo.co.uk>
2021-11-18 23:22:18 +0000

    print Voltage fixes

--- src/boot/ksdk1.1.0/devINA219.c
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -194,7 +194,7 @@ printSensorDataINA219(bool hexModeFlag, uint8_t address)
 		else if (address == 0x02)
 		{
 			Voltage = (readSensorRegisterValueCombined >> 3) * 4
-			warpPrint("Bus Voltage: %d mV,\n", V);
+			warpPrint("Bus Voltage: %d mV,\n", Voltage);
 		}
 		else
 		{
