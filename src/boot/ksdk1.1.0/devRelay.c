8b5ef953479cea9fb6fa6537542c5792de9357fb
Samuel2905 <samuelclarke2905@yahoo.co.uk>
2021-12-22 00:20:06 +0000

    change relay pins

--- src/boot/ksdk1.1.0/devRelay.c
+++ src/boot/ksdk1.1.0/devRelay.c
@@ -22,14 +22,23 @@
 enum
 {
 	kRelayPin	= GPIO_MAKE_PIN(HW_GPIOB, 1),
+	kRelayPin5	= GPIO_MAKE_PIN(HW_GPIOB, 5),
+	kRelayPin6	= GPIO_MAKE_PIN(HW_GPIOB, 6),
+	kRelayPin7	= GPIO_MAKE_PIN(HW_GPIOB, 7),
 };
 
 void initRelay(void) {
 	PORT_HAL_SetMuxMode(PORTB_BASE, 1, kPortMuxAsGpio);
+	PORT_HAL_SetMuxMode(PORTB_BASE, 5, kPortMuxAsGpio);
+	PORT_HAL_SetMuxMode(PORTB_BASE, 6, kPortMuxAsGpio);
+	PORT_HAL_SetMuxMode(PORTB_BASE, 7, kPortMuxAsGpio);
 }
 
 void TurnOnRelay(void) {
   GPIO_DRV_SetPinOutput(kRelayPin);
+	GPIO_DRV_SetPinOutput(kRelayPin5);
+	GPIO_DRV_SetPinOutput(kRelayPin6);
+	GPIO_DRV_SetPinOutput(kRelayPin7);
   warpPrint("Relay On\n");
 }
 
