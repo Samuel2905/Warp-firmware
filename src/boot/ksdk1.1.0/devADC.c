4aa07fdf331e97348c831e1c414ac2e71dc0246e
Samuel2905 <samuelclarke2905@yahoo.co.uk>
2021-12-22 00:40:44 +0000

    semi-colon

--- src/boot/ksdk1.1.0/devADC.c
+++ src/boot/ksdk1.1.0/devADC.c
@@ -38,7 +38,7 @@ void initADC(void) {
   MyChnConfig.chnNum = chn;
   MyChnConfig.diffEnable= false;
   MyChnConfig.intEnable = false;
-  warpPrint("ADC initialised")
+  warpPrint("ADC initialised");
 }
 
 void readADC(void) {
