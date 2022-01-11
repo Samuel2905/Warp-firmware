09464ea36838eeec7209dc5271f5e466f85385ca
Samuel2905 <samuelclarke2905@yahoo.co.uk>
2022-01-11 21:16:57 +0000

    find temperature calibration values

--- src/boot/ksdk1.1.0/devBME680.c
+++ src/boot/ksdk1.1.0/devBME680.c
@@ -164,6 +164,8 @@ WarpStatus
 configureSensorBME680(uint8_t payloadCtrl_Hum, uint8_t payloadCtrl_Meas, uint8_t payloadGas_0)
 {
 	uint8_t		reg, index, i = 0;
+	uint8_t		T1_msb, T1_lsb, T2_msb, T2_lsb, T3_all;
+	int16_t	T1_all, T2_all;
 	WarpStatus	status1, status2, status3, status4 = 0;
 
 
@@ -198,6 +200,25 @@ configureSensorBME680(uint8_t payloadCtrl_Hum, uint8_t payloadCtrl_Meas, uint8_t
 	for (i = 0; i < index; i++) {
 		warpPrint("Calib Data %u: %x\n", i, deviceBME680CalibrationValues[i] );
 	}
+
+	status = readSensorRegisterBME680(8A, 1 /* numberOfBytes */);
+	T2_lsb = deviceBME680State.i2cBuffer[0];
+	status = readSensorRegisterBME680(8B, 1 /* numberOfBytes */);
+	T2_msb = deviceBME680State.i2cBuffer[0];
+	status = readSensorRegisterBME680(8C, 1 /* numberOfBytes */);
+	T3_all = deviceBME680State.i2cBuffer[0];
+	status = readSensorRegisterBME680(E9, 1 /* numberOfBytes */);
+	T1_lsb = deviceBME680State.i2cBuffer[0];
+	status = readSensorRegisterBME680(EA, 1 /* numberOfBytes */);
+	T1_msb = deviceBME680State.i2cBuffer[0];
+
+	T1_all = ((T1_msb & 0xFF) << 8) | (T1_lsb);
+	T2_all = ((T2_msb & 0xFF) << 8) | (T2_lsb);
+	warpPrint("T1: %d\n", T1_all);
+	warpPrint("T2: %d\n", T2_all);
+	warpPrint("T3: %u\n", T3_all);
+
+
 
 	return (status1 | status2 | status3 | status4);
 }
